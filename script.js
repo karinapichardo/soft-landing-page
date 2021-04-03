7cd5fca67d5bd48acc767ebd7c7f0dc8cd7df307
karinapichardo <pichardokarina21@yahoo.com>
2021-04-02 23:58:46 -0400

    added functionality to list under features section

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ window.onscroll = () => {
     if (this.scrollY <= 10) nav.className = '';
     else nav.className = 'scroll';
 };
+
+function responsiveSection(responsive) {
+    document.querySelector('#responsive').style.display = 'block';
+    document.querySelector('#mobile').style.display = 'none';
+    document.querySelector('#support').style.display = 'none';
+}
+
+function mobileSection(mobile) {
+    document.querySelector('#responsive').style.display = 'none';
+    document.querySelector('#support').style.display = 'none';
+    document.querySelector('#mobile').style.display = 'block';
+}
+
+function supportSection(support) {
+    document.querySelector('#support').style.display = 'block';
+    document.querySelector('#responsive').style.display = 'none';
+    document.querySelector('#mobile').style.display = 'none';
+}
